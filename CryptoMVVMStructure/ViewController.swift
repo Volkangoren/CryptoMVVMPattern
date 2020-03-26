734dbc139d343a8159909b228b814ec3d9dd9ae0
Volkangoren <volkan_goren52@hotmail.com>
2020-03-26 03:16:51 +0300

    Initial Commit

---
+++ CryptoMVVMStructure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoMVVMStructure
+//
+//  Created by Volkan on 26.03.2020.
+//  Copyright © 2020 Volkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
